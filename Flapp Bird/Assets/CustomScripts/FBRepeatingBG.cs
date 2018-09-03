63105568381ff22f666c5c09e59e60e6f06c4618
masad26 <sheikhasad2662@gmail.com>
2018-09-03 17:22:44 +0500

    First Version of Flappy Bird App
    Flappy Bird App Demo

---
+++ Flapp Bird/Assets/CustomScripts/FBRepeatingBG.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FBRepeatingBG : MonoBehaviour {
+
+	private BoxCollider2D _boxCollider2d;
+
+	private float groundHorizontalLength;
+
+	// Use this for initialization
+	void Start () {
+		_boxCollider2d = GetComponent<BoxCollider2D> ();
+		groundHorizontalLength = _boxCollider2d.size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x < -groundHorizontalLength) {
+			repositionBackground ();
+		}
+	}
+
+	void repositionBackground() {
+		Vector2 groundOffset = new Vector2 (groundHorizontalLength * 2 , 0);
+		transform.position = (Vector2)transform.position + groundOffset;
+
+
+	}
+}
---
+++ Flapp Bird/Assets/CustomScripts/FlappyBirdGameController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FlappyBirdGameController : MonoBehaviour {
+
+	public static FlappyBirdGameController _fbgc;
+
+	public float scrollSpeed = -1.5f;
+	public bool isGameOver = false;
+	private int score = 0;
+
+	public Text score_text;
+	public GameObject gameOverText;
+
+
+	void Awake () {
+		if (_fbgc == null) {
+			_fbgc = this;
+		} else if (_fbgc != this) {
+			Destroy (gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (isGameOver && Input.GetMouseButtonDown (0)) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+		}
+	}
+
+	public void gameScore() 
+	{
+		if (isGameOver) {
+			return;
+		}
+		score++;
+		score_text.text = "Score : " + score;
+	}
+
+	public void playerDie() {
+		isGameOver = true;
+		gameOverText.SetActive (true);
+	}
+}
